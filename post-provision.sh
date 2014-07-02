f94e4c1a5b2b19ae10a7b740484a6c4a8e882f4f
Adam Argyle <atom@argyleink.com>
2014-07-02 13:41:38 -0700

    added console messaging for provision ux

--- post-provision.sh
+++ post-provision.sh
@@ -1,4 +1,7 @@
+echo -e "\033[1;32m STATUS: Post provisioning started \033[m"
+echo -e "\033[1;32m STATUS: Copying Nginx server file \033[m"
 sudo cp /var/server/app.conf /etc/nginx/sites-available/app
 sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled/app
 sudo nginx -s reload
+echo -e "\033[1;32m STATUS: Nginx done, starting Node server \033[m"
 node /var/api/app.js
--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
+echo -e "\033[1;32m STATUS: Installing NPM Modules \033[m"
 npm install
-bower install
 sudo npm install -g clientjade
+echo -e "\033[1;32m STATUS: Installing Bower Components \033[m"
+bower install
+echo -e "\033[1;32m STATUS: Grunt first run \033[m"
 grunt dev
+echo -e "\033[1;32m STATUS: Installing chef libs \033[m"
 librarian-chef install
+echo -e "\033[1;32m STATUS: Vagrant started, will be 2-5 minutes \033[m"
 vagrant up
+echo -e "\033[1;32m STATUS: Server ready, running grunt with watch \033[m"
+grunt
