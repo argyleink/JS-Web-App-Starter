d48649bbd6de893350e7e9bba1ecb3142bc87ebe
Adam Argyle <atom@argyleink.com>
2014-07-02 22:07:28 -0700

    node and mysql are playing nice

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
 var http      = require('http');
 var path      = require('path');
 var async     = require('async');
-var express   = require('express');
-var mysql     = require('mysql');
+var express   = require('express');     // http://expressjs.com/
+var mysql     = require('mysql');       // https://github.com/felixge/node-mysql
 
 var router = express().use(express.static(path.resolve(__dirname, 'client')));
 var server = http.createServer(router);
@@ -10,15 +10,17 @@ var server = http.createServer(router);
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : ''
+  password : 'password',
+  database : 'app'
 });
 
+// Example MySQL connection & query
 connection.connect(function(err) {
   if (err) throw err;
-  console.log(err);
+  console.log('Node has connected to MySQL');
 });
 
-connection.query('select * from user', function(err, rows, fields) {
+connection.query('select * from users', function(err, rows, fields) {
   if (err) throw err;
   console.log('Data row: ', rows[0]);
 });
@@ -30,7 +32,7 @@ server.listen(3000, "127.0.0.1", function(){
   console.log("Server listening at", addr.address + ":" + addr.port);
 });
 
-// test REST endpoint
+// Example REST endpoint
 router.get('/api/user/:id', function(req, res) {
   console.log(req.params);
   res.send('Sup ' + req.params.id);
